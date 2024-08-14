006915b90faefdcdf214822096db10b66daa7e60
Shiva Krishna <shivakrishna6032@gmail.com>
2024-08-14 23:15:50 +0530

    morris traversals

---
+++ Trees/22.flaten_bt_to_linkedlist.cpp
@@ -0,0 +1,26 @@
+
+
+void flatten(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+
+    TreeNode *curr = root;
+
+    while (curr != NULL)
+    {
+        if (curr->left != NULL)
+        {
+            TreeNode *prev = curr->left;
+            while (prev->right != NULL)
+            {
+                prev = prev->right;
+            }
+            prev->right = curr->right;
+            curr->right = curr->left;
+
+            curr->left = NULL;
+        }
+        curr = curr->right;
+    }
+}
